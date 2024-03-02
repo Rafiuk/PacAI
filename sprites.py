5f59a2949d73f71172143ad9dd968b3b3e5e554d
Rafiuk <r.fiuk@student.tudelft.nl>
2024-03-02 11:39:23 +0100

    Update sprites.py

--- sprites.py
+++ sprites.py
@@ -6,7 +6,7 @@ from animation import Animator
 BASETILEWIDTH = 16
 BASETILEHEIGHT = 16
 DEATH = 5
-
+TILEWIDTH
 class Spritesheet(object):
     def __init__(self):
         self.sheet = pygame.image.load("spritesheet.png").convert()
@@ -121,13 +121,13 @@ class GhostSprites(Spritesheet):
 
 
 class FruitSprites(Spritesheet):
-    def __init__(self, entity):
+    def __init__(self, entity, level):
         Spritesheet.__init__(self)
         self.entity = entity
         self.fruits = {0:(16,8), 1:(18,8), 2:(20,8), 3:(16,10), 4:(18,10), 5:(20,10)}
         self.entity.image = self.getStartImage(level % len(self.fruits))
 
-    def getStartImage(self):
+    def getStartImage(self, key):
         return self.getImage(*self.fruits[key])
 
     def getImage(self, x, y):
